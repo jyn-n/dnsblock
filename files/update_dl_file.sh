ec780efc7cb69ee6d84eb99f69a3076eea157998
jynn <uaggz@student.kit.edu>
2017-05-30 11:04:08 +0200

    added yoyo host

--- files/update_dl_file.sh
+++ files/update_dl_file.sh
@@ -1,16 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-url=http://someonewhocares.org/hosts/zero/hosts
-[[ -z $2 ]] || url=$2
+declare -A url
+url=( ['someonewhocares.dl']=http://someonewhocares.org/hosts/zero/hosts ['yoyo.dl']=http://pgl.yoyo.org/as/serverlist.php?mimetype=plaintext )
 
-tmpfile=$(mktemp)
+dlfile=$(mktemp)
+tmp1=$(mktemp)
+tmp2=$(mktemp)
 
-curl $url > $tmpfile
+curl ${url[$1]} > $dlfile
+
+case $1 in
+	'someonewhocares.dl')
+		sed '/#.*<localhost>/,/#.*<\/localhost>/d' < $dlfile | sed 's/#0.0.0.0/0.0.0.0/' | sed 's/#.*$//' | sed 's/^0.0.0.0 //' | sed 's/\s//g' | sed '/^$/d' | sort | uniq > $tmp1
+		;;
+	'yoyo.dl')
+		cat $dlfile > $tmp1
+		;;
+esac
 
 out=$1
 
 [[ -f $out ]] || touch $out
-[[ 0 -eq $(diff $tmpfile $out | wc -l) ]] || cp $tmpfile $out
 
-rm $tmpfile
+cat $tmp1 > $tmp2
+
+[[ -f $out ]] && cat $tmp1 $out | sort | uniq > $tmp2
+
+[[ 0 -eq $(diff $tmp2 $out | wc -l) ]] || cp $tmp2 $out
+
+rm $tmp1 $tmp2 $dlfile
 
