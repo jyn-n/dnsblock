a9e8fefc46ccb3cf1547ec5fcd5763d190b2c62d
jynn <uaggz@student.kit.edu>
2017-09-23 11:14:29 +0200

    added indentation to updatescript

--- update.sh
+++ update.sh
@@ -2,18 +2,18 @@
 
 git pull
 cd files
-make fetch_lists
-make -q && exit 0
-make
+	make fetch_lists
+	make -q && exit 0
+	make
 cd ..
 cd package
-makepkg
-cp PKGBUILD dnsblock-git
-cd dnsblock-git
-makepkg --printsrcinfo > .SRCINFO
-git commit -a -m "automatic update @ $(date)"
-git push
-cd ..
+	makepkg
+	cp PKGBUILD dnsblock-git
+	cd dnsblock-git
+		makepkg --printsrcinfo > .SRCINFO
+		git commit -a -m "automatic update @ $(date)"
+		git push
+	cd ..
 cd ..
 git commit -a -m "automatic update @ $(date)"
 git push
